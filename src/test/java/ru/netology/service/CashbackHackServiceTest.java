e1156896ae07911d462b0093c404e7821bdb2a5d
rmsmar <rms.marina89@gmail.com>
2021-11-12 23:34:21 +0300

    add tests to the branch  junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -12,6 +12,28 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn999For1001() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn100For900() {
+        int amount = 100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn0For1000() {
+        int amount =1000;
+        int actual = service.remain(amount);
+        int expected = 0;
         assertEquals(expected, actual);
     }
 }
