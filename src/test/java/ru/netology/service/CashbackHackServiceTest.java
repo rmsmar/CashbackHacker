d5434149f2e32ea0a6f09a91c73dbd10bc8cf8ed
rmsmar <rms.marina89@gmail.com>
2021-11-12 23:22:59 +0300

    add tests to the branch testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,30 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldReturn1000For0() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void shouldReturn999For1001() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn100For900() {
+        int amount = 100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
     }
 }
